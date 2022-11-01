b028e38650e28334c83586b7af8d03ee923c78a3
Thesaderror <88781785+thesaderror@users.noreply.github.com>
2022-11-01 21:37:26 +0400

    Delete _scripts directory

--- _scripts/webhook_spammer.py
+++
@@ -1,7 +0,0 @@
-from discord_webhook import DiscordWebhook
-
-def run_exploit(webhook,n,message):
-    webhook = DiscordWebhook(url=webhook, content=message)
-    while(n>0):
-        response = webhook.execute()
-        n-=1
